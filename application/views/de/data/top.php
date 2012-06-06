9376758d64af8fecbf6a073687def997fd2ff727
mik <mik@6a1f69d7-6018-4d13-bf90-b098e98c258d>
2012-06-06 14:04:20 +0000

    changed text

--- application/views/de/data/top.php
+++ application/views/de/data/top.php
@@ -1,6 +1,6 @@
 <div id="data_new">
     <h1>Liste der top Studien</h1><br/>
-    <p  class="normal">Die 20 meistverwendeten Studien , in absteigender Reihenfolge ihrer Download-Zahlen.</p>
+    <p  class="normal">Die 20 meistverwendeten Studien, in absteigender Reihenfolge ihrer Download-Zahlen.</p>
     <?= $list?>
 </div>
 <?= $dialog ?>
