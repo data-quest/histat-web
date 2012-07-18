8aba49ff41d01053fea9816305aba89014f46d67
root <root@6a1f69d7-6018-4d13-bf90-b098e98c258d>
2012-07-18 11:41:11 +0000

    Galerie

---
+++ application/views/de/pages/galery.php
@@ -0,0 +1,18 @@
+<div id="content" class="ui-corner-bottom">
+    <div id="home">
+
+        <div class="left" style="border:0px">
+            <div id="welcome" style="border:0px"><h1>Galerie</h1><br/>
+                <h3>Datenbank zur historischen Statistik</h3>
+                <p class="normal">
+                    Die Galerie mit ausgewählten Graphiken zu einzelnen Studien befindet sich in der Vorbereitung. 
+                </p>
+        </div>
+    </div>
+
+</div>
+<script type="text/javascript">
+    $(function(){
+        $('#gotop').remove(); 
+    })
+</script>
