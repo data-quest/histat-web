b2cb83357d63052c194591b95bc1b23a85e299b2
rahlf <thomas.rahlf@gesis.org>
2012-12-06 11:17:30 +0100

    title changed

--- application/views/de/pages/welcome.php
+++ application/views/de/pages/welcome.php
@@ -1,7 +1,7 @@
-<h1><span class="orange">histat</span><br/>Zeitreihen zur Historischen Statistik</h1><br/>
+<h1>Zeitreihen zur Historischen Statistik</h1><br/>
 <div class="startimage"></div>
 <p class="normal">
-Willkommen bei der Online-Datenbank histat. Die Datenbank wurde 2004 freigeschaltet und 2012 grundlegend überarbeitet. Die Anzahl der Datenbankaufrufe hat sich innerhalb von zwei Jahren mehr als verdoppelt. Dieses Jahr werden wir über 3.000 registrierte Nutzerinnen und Nutzer verzeichnen. Die Datenbank umfasst mittlerweile eine Viertelmillion Zeitreihen aus 359 Studien mit insgesamt über fünf Millionen Werten.</p>
+Willkommen bei der Online-Datenbank <span class="orange">histat</span>. Die Datenbank wurde 2004 freigeschaltet und 2012 grundlegend überarbeitet. Die Anzahl der Datenbankaufrufe hat sich innerhalb von zwei Jahren mehr als verdoppelt. Dieses Jahr werden wir über 3.000 registrierte Nutzerinnen und Nutzer verzeichnen. Die Datenbank umfasst mittlerweile eine Viertelmillion Zeitreihen aus 359 Studien mit insgesamt über fünf Millionen Werten.</p>
 <h3>Zitierweise</h3>
 <p class="normal">
 Bitte zitieren Sie die Quelle der verwendeten Daten und die zu den Daten angegebenen Veröffentlichungen in den eigenen Publikationen entsprechend den wissenschaftlichen Gepflogenheiten. Empfehlungen zur Zitierweise von historischen Daten sind unter <?= HTML::anchor('http://www.gesis.org/unser-angebot/daten-analysieren/datenservice/forschungsdaten-zitieren/','Datenzitat',array('target'=>'blank')) ?> zu finden.
