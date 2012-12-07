72589ad14c79ad96ce808e8ed8d3b64fc112f1b1
rahlf <thomas.rahlf@gesis.org>
2012-12-07 12:05:58 +0100

    title and link changed

--- application/views/en/pages/welcome.php
+++ application/views/en/pages/welcome.php
@@ -1,7 +1,7 @@
-<h1><span class="orange">histat</span>  Zeitreihen zur Historischen Statistik online</h1><br/>
+<h1>Historical Time Series</h1><br/>
 <div class="startimage"></div>
 <p class="normal">
-Welcome to the online-database histat. The database has been offered via GESIS since 2004. A substantial revision has been made in 2012. The number of database calls has been more than doubled within the last two years. This year (in 2012) we will attain more than 3.000 registered users. In the meantime, the database encloses a quarter of a million time series, 359 studies, and more than five million values. </p>
+Welcome to the online-database <span class="orange">histat</span>. The database has been offered via GESIS since 2004. A substantial revision has been made in 2012. The number of database calls has been more than doubled within the last two years. This year (in 2012) we will attain more than 3.000 registered users. In the meantime, the database encloses a quarter of a million time series, 359 studies, and more than five million values. </p>
 <h3>Citation</h3>
 <p class="normal">
-    Kindly cite the sources of the used data and the respective publication of the downloaded studies in your own publication according to the scientific conventions. Recommendations regarding the citation of historical data can be found under the topic <?= HTML::anchor('http://www.gesis.org/en/services/data-analysis/data-archive-service/citation-of-research-data/','"Bibliographic citation of research data and study related documents"',array('target'=>'blank'))?>.</p>
+    Kindly cite the sources of the used data and the respective publication of the downloaded studies in your own publication according to the scientific conventions. Recommendations regarding the citation of historical data can be found under the topic <?= HTML::anchor('http://www.gesis.org/en/services/data-analysis/daten-historischer-studien/histat/','"Bibliographic citation of research data and study related documents"',array('target'=>'blank'))?>.</p>
