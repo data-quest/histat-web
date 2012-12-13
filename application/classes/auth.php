e6c4e283beb4c81d72280f7f8df4f2e60ade43a1
BlackScorp <witalimik@web.de>
2012-12-13 14:49:55 +0100

    testing auth

--- application/classes/auth.php
+++ application/classes/auth.php
@@ -11,10 +11,14 @@ abstract class Auth extends Kohana_Auth {
         if (!$user) {
             return $default;
         } else {
+            try{
             $user->ip = $_SERVER['REMOTE_ADDR'];
             $user->last_action = time();          
             $user->update();
             return $user;
+            }catch(ORM_Validation_Exception $e){
+                Debug::vars($e->errors(I18n::$lang));
+            }
         }
     }
 
