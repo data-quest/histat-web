34f76fa377146c256e7fe6a39a206550174ff64e
BlackScorp <witalimik@web.de>
2013-02-20 10:13:19 +0100

    fix #1 testing to close issuses with commits

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,6 +1,4 @@
-<?php
-
-defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') or die('No direct script access.');
 
 // -- Environment setup --------------------------------------------------------
 // Load the core Kohana class
