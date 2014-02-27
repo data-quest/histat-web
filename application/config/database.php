3305033cc448596eecc2f4aed1d42817448235fc
BlackScorp <witalimik@web.de>
2014-02-27 12:49:03 +0100

    ignore configs

---
+++ application/config/database.php
@@ -0,0 +1,35 @@
+<?php
+
+defined('SYSPATH') or die('No direct access allowed.');
+
+return array
+    (
+    'default' => array(
+        'type' => 'pdo',
+        'connection' => array(
+            /**
+             * The following options are available for PDO:
+             *
+             * string   dsn         Data Source Name
+             * string   username    database username
+             * string   password    database password
+             * boolean  persistent  use persistent connections?
+             */
+            'dsn' => 'mysql:host=localhost;dbname=mydb',
+            'username' => 'user',
+            'password' => 'password',
+            'persistent' => FALSE,
+        ),
+        /**
+         * The following extra options are available for PDO:
+         *
+         * string   identifier  set the escaping identifier
+         */
+        'table_prefix' => '',
+       // 'identifier' => '`',
+        'charset' => 'utf8',
+        'caching' => FALSE,
+        'profiling' => (Kohana::$environment !== Kohana::PRODUCTION),
+    ),
+  
+);
