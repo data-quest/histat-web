b33ff00033a1b2ceabebc885f6d92e24addae88d
Witali <Witali@PC-WITALI>
2014-05-12 13:16:40 +0200

    path der downloads geändert

--- application/classes/controller/cart.php
+++ application/classes/controller/cart.php
@@ -11,41 +11,41 @@ class Controller_Cart extends Controller_Table {
 
     public function action_index() {
         $view = View::factory(I18n::$lang . '/cart/index');
-     
 
-        $filters = array();
+
+        $filters  = array();
         $projects = array();
-        $tables = array();
-     
-        foreach ($this->user->getCartItems() as $item) {   
-        
+        $tables   = array();
+
+        foreach ($this->user->getCartItems() as $item) {
+
             $bearbeitung = '';
-            $datum = substr($item->Datum_der_Bearbeitung, -4);
+            $datum       = substr($item->Datum_der_Bearbeitung, -4);
             if (!empty($datum)) {
                 $bearbeitung = '[' . $datum . ']';
             }
-            $projectName = __(':author, (:pub_year :edit_year) :project', array(':author' => $item->Projektautor,
-                ':pub_year' => $item->Publikationsjahr,
+            $projectName                                             = __(':author, (:pub_year :edit_year) :project', array(':author'    => $item->Projektautor,
+                ':pub_year'  => $item->Publikationsjahr,
                 ':edit_year' => $bearbeitung,
-                ':project' => $item->Projektname
-                    ));
-            $filters[$item->ID_Projekt][$item->ID_HS][$item->filter] = array('text' =>  json_decode($item->filter_text), 'timelines' => $item->timelines);
-            $projects[$item->ID_Projekt] = array('name' => $projectName,
-                'za' => $item->ZA_Studiennummer,
+                ':project'   => $item->Projektname
+            ));
+            $filters[$item->ID_Projekt][$item->ID_HS][$item->filter] = array('text' => json_decode($item->filter_text), 'timelines' => $item->timelines);
+            $projects[$item->ID_Projekt]                             = array('name'  => $projectName,
+                'za'    => $item->ZA_Studiennummer,
                 'theme' => $item->Thema
             );
 
             $tables[$item->ID_Projekt][$item->ID_HS] = $item->Name;
         }
-      
- 
-     
-        $view->message = $this->request->param('id', FALSE);
-        $view->projects =$projects;
-        $view->tables = $tables;
-        $view->filters = $filters;
-        $view->options = Kohana::$config->load('download')->get('options');
-        $this->content = $view->render();
+
+
+
+        $view->message  = $this->request->param('id', FALSE);
+        $view->projects = $projects;
+        $view->tables   = $tables;
+        $view->filters  = $filters;
+        $view->options  = Kohana::$config->load('download')->get('options');
+        $this->content  = $view->render();
     }
 
     public function action_delete() {
@@ -63,18 +63,18 @@ class Controller_Cart extends Controller_Table {
     }
 
     public function action_delete_selected() {
-        $selected =  $this->request->post('selected');
-        if($selected){
-        $result = DB::delete('warenkorb')
-                ->where('user_id', '=', $this->user->id)
-                ->where(DB::expr('CONCAT_WS("/",ID_HS,filter)'), 'IN', DB::expr("('" . implode("','", $selected) . "')"))
-                ->execute();
+        $selected = $this->request->post('selected');
+        if ($selected) {
+            $result = DB::delete('warenkorb')
+                    ->where('user_id', '=', $this->user->id)
+                    ->where(DB::expr('CONCAT_WS("/",ID_HS,filter)'), 'IN', DB::expr("('" . implode("','", $selected) . "')"))
+                    ->execute();
 
 
-        if ($result) {
+            if ($result) {
 
-            $this->request->redirect(I18n::$lang . '/cart/index/success');
-        }
+                $this->request->redirect(I18n::$lang . '/cart/index/success');
+            }
         }
         $this->request->redirect(I18n::$lang . '/cart/index/error');
     }
@@ -84,8 +84,8 @@ class Controller_Cart extends Controller_Table {
             throw new HTTP_Exception_404();
         }
         $this->auto_render = FALSE;
-        $this->id_hs = $this->request->post('id');
-        $this->filter = $this->request->post('filter');
+        $this->id_hs       = $this->request->post('id');
+        $this->filter      = $this->request->post('filter');
 
         $filter_text = json_encode($this->request->post('filter_text'));
 
@@ -95,13 +95,13 @@ class Controller_Cart extends Controller_Table {
                 ->find_all();
 
         if ($cart->count() == 0) {
-            $cart = ORM::factory('cart');
-            $cart->ID_HS = $this->id_hs;
-            $cart->filter = $this->filter;
-            $cart->timelines = $this->request->post('timelines');
+            $cart              = ORM::factory('cart');
+            $cart->ID_HS       = $this->id_hs;
+            $cart->filter      = $this->filter;
+            $cart->timelines   = $this->request->post('timelines');
             $cart->filter_text = $filter_text;
-            $cart->user_id = $this->user->id;
-            $cart->chdate = time();
+            $cart->user_id     = $this->user->id;
+            $cart->chdate      = time();
             $cart->create();
             echo true;
         }
@@ -114,73 +114,74 @@ class Controller_Cart extends Controller_Table {
         if (HTTP_Request::POST != $this->request->method() || $this->request->post('selected') == NULL) {
             $this->request->redirect(I18n::$lang . '/cart/index');
         }
-       
+
         $cart = ORM::factory('cart')
                 ->where('user_id', '=', $this->user->id)
                 ->where(DB::expr('CONCAT_WS("/",keymask.ID_HS,filter)'), 'IN', DB::expr("('" . implode("','", $this->request->post('selected')) . "')"))
                 ->find_all();
-       
+
         $formats = array(
-            'xsl' => 'Excel5',
+            'xsl'  => 'Excel5',
             'xslx' => 'Excel2007',
-            'csv' => 'CSV'
+            'csv'  => 'CSV'
         );
-        if (!is_dir('/tmp/histat/')) {
-            mkdir('/tmp/histat/', 0777, true);
+        $path    = realpath(__DIR__ . '/../../../../');
+        //$path    = '/tmp/histat';
+
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
         }
-        if (!is_dir('/tmp/histat/download_' . $this->user->id)) {
-            mkdir('/tmp/histat/download_' . $this->user->id, 0777, true);
+        if (!is_dir($path . '/download_' . $this->user->id)) {
+            mkdir($path . '/download_' . $this->user->id, 0777, true);
         }
         $uses = $this->request->post('uses');
-        if ($uses == '-1')
-            $uses = $this->request->post('custom');
+        if ($uses == '-1') $uses = $this->request->post('custom');
 
 
 
-       
+
         foreach ($cart as $nr => $item) {
 
-            $keymask = ORM::factory('keymask', $item->ID_HS);
-                   $table_name = substr(str_replace(array('"',':',' ','/','\\','.') , '_',  $keymask->Name),0,100);
-            $details = $keymask->getDetails($item->filter);
+            $keymask         = ORM::factory('keymask', $item->ID_HS);
+            $table_name      = substr(str_replace(array('"', ':', ' ', '/', '\\', '.'), '_', $keymask->Name), 0, 100);
+            $details         = $keymask->getDetails($item->filter);
             $details['data'] = null;
-            $names = array();
+            $names           = array();
             if (count(Arr::get($details, 'keys', array())) <= $this->config->get('max_timelines')) {
                 $details['data'] = $keymask->getData($item->filter);
             }
-            $download = ORM::factory('download');
-            $download->username = $this->user->username;
-            $download->projekt_id = $keymask->project->ID_Projekt;
-            $download->anzahl = count($details['keys']);
-            $download->type =  $this->request->post('format');
+            $download               = ORM::factory('download');
+            $download->username     = $this->user->username;
+            $download->projekt_id   = $keymask->project->ID_Projekt;
+            $download->anzahl       = count($details['keys']);
+            $download->type         = $this->request->post('format');
             $download->intended_use = $uses;
-            $download->za_nummer = $keymask->project->ZA_Studiennummer;
-            $download->name = $keymask->project->Projektname;
-            $download->mkdate = time();
+            $download->za_nummer    = $keymask->project->ZA_Studiennummer;
+            $download->name         = $keymask->project->Projektname;
+            $download->mkdate       = time();
             $download->create();
 
             if ($details['data']) {
-                
-                $grid = $this->create_grid($details);
+
+                $grid    = $this->create_grid($details);
                 $grid[1] = array($table_name);
-                $ws = new Spreadsheet();
+                $ws      = new Spreadsheet();
                 $ws->set_active_sheet(0);
                 $ws->set_data($grid);
-                $name = iconv('UTF-8', 'ASCII//TRANSLIT',$table_name . '-' . $nr);
-                $name = $ws->save(array('name' => $name, 'format' => Arr::get($formats, $this->request->post('format'), 'Excel2007'), 'path' => '/tmp/histat/download_' . $this->user->id . '/'));
+                $name    = iconv('UTF-8', 'ASCII//TRANSLIT', $table_name . '-' . $nr);
+                $name    = $ws->save(array('name' => $name, 'format' => Arr::get($formats, $this->request->post('format'), 'Excel2007'), 'path' => $path . '/download_' . $this->user->id . '/'));
             }
         }
-        $path = '/tmp/histat/';
 
 
-        $command = sprintf("cd /tmp/histat/ ;zip -r download_%d.zip  download_%d/", $this->user->id, $this->user->id);
+        $command = sprintf("cd " . $path . " ;zip -r download_%d.zip  download_%d/", $this->user->id, $this->user->id);
         $output  = array();
-         exec($command, $output);
-     
-        //$this->rrmdir('/tmp/histat/download_' . $this->user->id . '/');
+        exec($command, $output);
+
+        //$this->rrmdir($path . '/download_' . $this->user->id . '/');
 
 
-        $this->response->send_file('/tmp/histat/download_' . $this->user->id . '.zip', sprintf("histat.gesis.org_Warenkorb_%s.zip", date("m-d-y-h-i", time())), array('delete' => false));
+        $this->response->send_file($path . '/download_' . $this->user->id . '.zip', sprintf("histat.gesis.org_Warenkorb_%s.zip", date("m-d-y-h-i", time())), array('delete' => false));
     }
 
     private function rrmdir($dir) {
@@ -202,4 +203,3 @@ class Controller_Cart extends Controller_Table {
     }
 
 }
-
